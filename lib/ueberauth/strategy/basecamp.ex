354bcc942249f184a773fc9463f0cf7ff6197348
Matt Mueller <muellermr@gmail.com>
2017-01-05 14:35:28 -0500

    Make sure we define web_server type.

--- lib/ueberauth/strategy/basecamp.ex
+++ lib/ueberauth/strategy/basecamp.ex
@@ -50,10 +50,7 @@ defmodule Ueberauth.Strategy.Basecamp do
   Handles the initial redirect to the basecamp authentication page.
   """
   def handle_request!(conn) do
-    opts = [redirect_uri: callback_url(conn)]
-
-    opts =
-      if conn.params["state"], do: Keyword.put(opts, :state, conn.params["state"]), else: opts
+    opts = [redirect_uri: callback_url(conn), type: 'web_server']
 
     module = option(conn, :oauth2_module)
     redirect!(conn, apply(module, :authorize_url!, [opts]))
